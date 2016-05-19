df16679162dfb2b1dfe01922ce1180a574a55b07
JNFC-yangdianwen <2550286303@qq.com>
2016-05-19 16:39:11 +0800

    增加了动画效果，

--- app/src/main/java/com/example/yangdianwen/handlerdemo/MainActivity.java
+++ app/src/main/java/com/example/yangdianwen/handlerdemo/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.yangdianwen.handlerdemo;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private int index;
+    //图片资源
     private int[] images = {R.mipmap.ic_launcher, R.mipmap.and, R.mipmap.logo_5};
+    //动画资源
+    private int[]animation={R.anim.alpha,R.anim.scale,R.anim.translate};
     private ImageView imageView;
     MyRunnable myRunnable = new MyRunnable();
     private Handler mhandler = new Handler();
@@ -21,8 +26,10 @@ public class MainActivity extends AppCompatActivity {
             //index对images数组的长度取余实现了图片之间不停切换效果（类似循环）。
             index = index % 3;
             imageView.setImageResource(images[index]);
+            Animation anim= AnimationUtils.loadAnimation(MainActivity.this,animation[index]);
+            imageView.setAnimation(anim);
             //使用Handler对象调用postDelayed方法，实现了延时效果。
-            mhandler.postDelayed(myRunnable, 500);
+            mhandler.postDelayed(myRunnable, 1500);
         }
     }
 
@@ -32,6 +39,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageView = (ImageView) findViewById(R.id.image);
         //在UI线程中使用Handler对象调用postDelayed方法实现图片的切换。
-        mhandler.postDelayed(myRunnable, 100);
+        mhandler.postDelayed(myRunnable, 1000);
     }
 }
